688e5c1e70c4d33e04a7d116bcf4bb6f5db6d9f4
Cristina Baiu <115459671+FineLine01@users.noreply.github.com>
2023-02-15 10:01:06 +0000

    css-design

--- js/myScript.js
+++ js/myScript.js
@@ -32,3 +32,27 @@ let title = document.querySelector("#title").innerHTML = `<span>Title: </span><s
 let botany = document.querySelector("#botany").innerHTML = `<span>Botany: </span><span>${lotus.botany}</span>`
 
 let symbol = document.querySelector("#symbol").innerHTML = `Symbol: ${lotus.symbol}`
+
+const plumeria = {
+    title_p: 'Plumeria',
+    botany_p: 'Full sun, well-drained soil',
+    symbol_p: 'Love'
+}
+
+let title_p = document.querySelector("#title-p").innerHTML = `Title: ${plumeria.title_p}`
+
+let botany_p = document.querySelector("#botany-p").innerHTML = `Botany: ${plumeria.botany_p}`
+
+let symbol_p = document.querySelector("#symbol-p").innerHTML = `Symbol: ${plumeria.symbol_p}`
+
+const lily = {
+    title_l: 'Zantedeschia',
+    botany_l: 'Marshy areas',
+    symbol_l: 'Love'
+}
+
+let title_l = document.querySelector("#title-l").innerHTML = `Title: ${lily.title_l}`
+
+let botany_l = document.querySelector("#botany-l").innerHTML = `Botany: ${lily.botany_l}`
+
+let symbol_l = document.querySelector("#symbol-l").innerHTML = `Symbol: ${lily.symbol_l}`
